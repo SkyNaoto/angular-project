57a0e9475e82ac0947ef366b03bfc3707eaba6d0
SkyNaoto <naoto_70102@yahoo.co.jp>
2024-10-21 07:22:59 +0900

    dev5

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  public categoryList: string[] = ['Fridge', 'Washer', 'Kitchen', 'Vacuum', 'Climate', 'TV', 'Other'];
+
+  public trackByIndex(index: number, item: any): number {
+    return index;
+  }
+
+}
